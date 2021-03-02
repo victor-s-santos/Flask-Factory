f229071e438bc5c3dbfe10885877b299c41a1a1d
Victor Santos Silva <victor.s.silva@unesp.br>
2021-03-02 12:46:58 -0300

    fix conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
-from ... blog.app import create_app
+from blog.app import create_app
 
 @pytest.fixture(scope='module')
 def app():
+    """Init the application"""
     return create_app()
